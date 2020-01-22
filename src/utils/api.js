0d41eb6cc100e673d7bfe376fef1e2ad0ce62864
John <johnrubio93@gmail.com>
2020-01-22 23:15:52 +1100

    created fetch functions for api

---
+++ src/utils/api.js
@@ -0,0 +1,28 @@
+const headers = {
+  'Content-Type': 'application/json',
+  'x-app-id': 'de0ce94b',
+  'x-app-key': '9b3909262e3eb687c907eb72099b7fed',
+};
+
+export const requestFood = async (query) => {
+  const url = `https://trackapi.nutritionix.com/v2/search/instant?query=${query}`;
+  const response = await fetch(url, {
+    method: 'GET',
+    headers,
+  });
+  const data = await response.json();
+  console.log('API requestFood', data);
+  return data;
+};
+
+export const requestFoodDetails = async (query) => {
+  const url = 'https://trackapi.nutritionix.com/v2/natural/nutrients';
+  const response = await fetch(url, {
+    method: 'POST',
+    headers,
+    body: JSON.stringify({ query }),
+  });
+  const data = await response.json();
+  console.log('API requestFoodDetails', data);
+  return data;
+};
