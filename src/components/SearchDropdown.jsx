921891c6fa566c62a932eec933ed20bf23bf7628
John <johnrubio93@gmail.com>
2020-01-26 00:06:50 +1100

    added dropdown overflow scroll

--- src/components/SearchDropdown.jsx
+++ src/components/SearchDropdown.jsx
@@ -4,14 +4,17 @@ import { Divider, Paper } from '@material-ui/core';
 import FoodList from './FoodList';
 
 const useStyles = makeStyles({
-
+  dropdown: {
+    maxHeight: '90vh',
+    overflowY: 'scroll',
+  },
 });
 
 const SearchDropdown = ({ commonFood, brandedFood }) => {
   const classes = useStyles();
 
   return (
-    <Paper>
+    <Paper className={classes.dropdown}>
       {commonFood.length ? <FoodList title="Common" foodList={commonFood} /> : null}
       {commonFood.length && brandedFood.length ? <Divider /> : null}
       {brandedFood.length ? <FoodList title="Branded" foodList={brandedFood} /> : null}
