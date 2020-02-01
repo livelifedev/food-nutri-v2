a42dfc4f0ba29417c54ddb0eb9a1305d78930b27
John <johnrubio93@gmail.com>
2020-02-02 01:55:50 +1100

    split sidebar to multiple components

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,37 +1,11 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import {
-  Avatar, Typography, Divider, Box,
-} from '@material-ui/core';
+import { Typography, Divider, Box } from '@material-ui/core';
+import ProfileDisplay from './ProfileDisplay';
 import { mapCalories, calcPercent } from '../utils/helpers';
 import mock from '../mock';
 
 const useStyles = makeStyles({
-  avatarItems: {
-    display: 'flex',
-    justifyContent: 'space-evenly',
-    alignItems: 'center',
-    height: '130px',
-    paddingTop: '20px',
-  },
-  avatar: {
-    width: '100px',
-    height: '100px',
-  },
-  dataCircle: {
-    backgroundColor: '#7A7A7A',
-    width: '50px',
-    height: '50px',
-    borderRadius: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  dataText: {
-    lineHeight: '1',
-    color: '#FFFFFF',
-  },
   dataTextTop: {
     fontWeight: '500',
     lineHeight: '1.3',
@@ -87,31 +61,14 @@ const useStyles = makeStyles({
 
 const Sidebar = ({ intakeData, cycle, todaysIntake }) => {
   const classes = useStyles();
-  const {
-    first_name: firstName,
-    last_name: lastName,
-    height_cm: heightCm,
-    weight_kg: weightKg,
-    daily_goal: dailyGoal,
-  } = mock;
+  const { daily_goal: dailyGoal } = mock;
   const intake = cycle ? intakeData[cycle].intake_list : todaysIntake;
   const caloriesMap = mapCalories(intake);
   const percentage = calcPercent(caloriesMap.totalCals, dailyGoal);
 
   return (
     <>
-      <Box className={classes.avatarItems}>
-        <Box className={classes.dataCircle}>
-          <Typography className={classes.dataText} variant="body1">{weightKg}</Typography>
-          <Typography className={classes.dataText} variant="caption">kg</Typography>
-        </Box>
-        <Avatar alt={`${firstName} ${lastName}`} src="" className={classes.avatar} />
-        <Box className={classes.dataCircle}>
-          <Typography className={classes.dataText} variant="body1">{heightCm}</Typography>
-          <Typography className={classes.dataText} variant="caption">cm</Typography>
-        </Box>
-      </Box>
-      <Typography component="h2" variant="h5" align="center">{`${firstName} ${lastName}`}</Typography>
+      <ProfileDisplay />
 
       <Divider className={classes.divider} />
 
